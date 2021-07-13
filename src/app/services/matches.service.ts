067b536db49d0b1c28ade4b3ae56f1cb5b46ba32
Alberto-098 <alberto.villalobos1@ulatina.net>
2021-07-13 12:14:11 -0600

    styles for app

--- src/app/services/matches.service.ts
+++ src/app/services/matches.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,46 +9,77 @@ export class MatchesService {
   private matches = [
     {
       id: 1,
-      teamA : 'Costa Rica',
-      teamB : 'Uruguay',
+      teamA : 'FC Barcelona',
+      logoTeamA : 'http://assets.stickpng.com/images/584a9b3bb080d7616d298777.png',
+      teamB : 'Real Madrid',
+      logoTeamB : 'http://assets.stickpng.com/images/584a9b47b080d7616d298778.png',
       finished: false,
-      score: '3 - 1',
+      score: '2 - 1',
       events: [
-        'Gol Cavanni al min 24',
+        {minute: 0+ ' min', event: ' Inicio del Partido', image: 'https://image.flaticon.com/icons/png/512/2213/2213579.png'},
+        {minute: 24+' min', event: ' Goool de Messi', image: 'https://image.flaticon.com/icons/png/512/1800/1800944.png'},
+        {minute: 29+' min', event: ' Tarjeta Amarilla a Benzema', image: 'https://cdn.iconscout.com/icon/free/png-256/yellow-card-1911102-1613794.png'},
+        {minute: 54+' min', event: ' Goool de Bale', image: 'https://image.flaticon.com/icons/png/512/1800/1800944.png'},
+        {minute: 57+' min', event: ' Tarjeta Roja a Pique', image: 'https://image.flaticon.com/icons/png/512/451/451718.png'},
+        {minute: 75+' min', event: ' Goool de Griezmann', image: 'https://image.flaticon.com/icons/png/512/1800/1800944.png'},
+
+        /*'Gol Cavanni al min 24',
         'Tarjeta amarilla para Lugano al min 29',
         'Gol Campbell al min 54',
         'Gol Duarte al min 57',
-        'Gol Ureña al min 86',
+        'Gol Ureña al min 86',*/
       ]
     },
     {
       id: 2,
-      teamA : 'Costa Rica',
-      teamB : 'Italia',
+      teamA : 'FC Bayern',
+      logoTeamA : 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bayern_M%C3%BCnchen_Logo_%281996-2002%29.svg/1200px-Bayern_M%C3%BCnchen_Logo_%281996-2002%29.svg.png' ,
+      teamB : 'Chelsea',
+      logoTeamB : 'https://logodownload.org/wp-content/uploads/2017/02/chelsea-fc-logo-1.png',
       finished: true,
       score: '1 - 0',
       events: [
-        'Gol Ruiz al min 41',
+        {minute: 0+  ' min', event: ' Inicio del Partido', image: 'https://image.flaticon.com/icons/png/512/2213/2213579.png'},
+        {minute: 26+ ' min', event: ' Tiro Libre para el Bayern', image: 'https://image.flaticon.com/icons/png/512/867/867329.png'},
+        {minute: 45+ ' min', event: ' Remate y Goool de Lewandowski', image: 'https://image.flaticon.com/icons/png/512/861/861512.png'},
+        {minute: 70+ ' min', event: ' Gran atajada x parte del Chelsea', image: 'https://image.flaticon.com/icons/png/512/1654/1654404.png'},
+        {minute: 90+ ' min', event: ' Partido Finalizado', image: 'https://image.flaticon.com/icons/png/512/1505/1505471.png'}
       ]
     },
     {
       id: 3,
-      teamA : 'Costa Rica',
-      teamB : 'Inglaterra',
+      teamA : 'PSG',
+      logoTeamA : 'http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/49.png' ,
+      teamB : 'Juventus',
+      logoTeamB : 'https://a2.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fsoccer%2F500%2F111.png',
       finished: true,
-      score: '0 - 0',
+      score: '1 - 2',
       events: [
-        'Penal para Costa Rica al minuto 12',
+        {minute: 0+ ' min', event: ' Inicio del Partido', image: 'https://image.flaticon.com/icons/png/512/2213/2213579.png'},
+        {minute: 24+' min', event: ' Goool de Cristiano Ronaldo', image: 'https://image.flaticon.com/icons/png/512/1800/1800944.png'},
+        {minute: 57+' min', event: ' Tarjeta Amarilla a Mbappe', image: 'https://image.flaticon.com/icons/png/512/3363/3363491.png'},
+        {minute: 54+' min', event: ' Gooolazo de Neymar', image: 'https://image.flaticon.com/icons/png/512/2726/2726007.png'},
+        {minute: 29+' min', event: ' Cambio de Jugadores', image: 'https://image.flaticon.com/icons/png/512/861/861522.png'},
+        {minute: 86+' min', event: ' Goool del gran Dybala', image: 'https://image.flaticon.com/icons/png/512/1800/1800944.png'},
+        {minute: 90+ ' min', event: ' Partido Finalizado', image: 'https://image.flaticon.com/icons/png/512/1505/1505471.png'}
       ]
     },
     {
       id: 4,
-      teamA : 'Costa Rica',
-      teamB : 'Grecia',
+      teamA : 'Inter',
+      logoTeamA : 'http://assets.stickpng.com/images/584aaf7130d6736bca3d6ded.png' ,
+      teamB : 'Atletico Madrid',
+      logoTeamB : 'https://logodownload.org/wp-content/uploads/2017/02/atletico-madrid-logo-1.png',
       finished: true,
       score: '1 - 1',
       events: [
-        'Gol de Ruiz al min 14',
+        {minute: 0+  ' min', event: ' Inicio del Partido', image: 'https://image.flaticon.com/icons/png/512/2213/2213579.png'},
+        {minute: 26+ ' min', event: ' Tiro de Esquina para el Inter', image: 'https://image.flaticon.com/icons/png/512/3100/3100988.png'},
+        {minute: 45+ ' min', event: ' Remate y Goool de Luis Suarez', image: 'https://image.flaticon.com/icons/png/512/861/861512.png'},
+        {minute: 60+ ' min', event: ' Gran atajada x parte de Oblak', image: 'https://image.flaticon.com/icons/png/512/1654/1654404.png'},
+        {minute: 82+ ' min', event: ' Goool de tiro libre x parte de Lukaku', image: 'https://image.flaticon.com/icons/png/512/2726/2726007.png'},
+        {minute: 82+ ' min', event: ' Fuera de Juego, Gol Anulado del Atletico', image: 'https://image.flaticon.com/icons/png/512/2817/2817725.png'},
+        {minute: 90+ ' min', event: ' Y esto termina en un Empate', image: 'https://image.flaticon.com/icons/png/512/1505/1505471.png'}
       ]
     }
   ]
